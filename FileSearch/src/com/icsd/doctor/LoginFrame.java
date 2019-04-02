b9974f4a6475c316a3102773983762faf1e8e667
Sanchit Khurana <sanchit007khurana@gmail.com>
2019-04-02 22:03:57 +0530

    Project Initilized
    Signed-off-by: Sanchit Khurana <sanchit007khurana@gmail.com>

---
+++ FileSearch/src/com/icsd/doctor/LoginFrame.java
@@ -0,0 +1,24 @@
+package com.icsd.doctor;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ * @author Sanchit
+ *
+ */
+public class LoginFrame extends JFrame{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public LoginFrame() {
+		
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		this.setSize(new Dimension(600, 600));
+	}
+}
